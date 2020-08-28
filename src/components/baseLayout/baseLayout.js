a15c42c73b6e28059938d636762c2c8623e1ef0e
IP777 <ivanov_piter@ukr.net>
2020-08-28 13:10:37 +0300

    ed-3

--- src/components/baseLayout/baseLayout.js
+++ src/components/baseLayout/baseLayout.js
@@ -3,36 +3,28 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 // import Container from '@material-ui/core/Container';
 
 export default function BaseLayout(props) {
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <React.Fragment>
-                <div className={style.mainWrapper}>
-
-                    <div className={style.header}>Here heder component</div>
-                    <div className={style.wrapper}>
-                        <div className={style.wrapperSidebar}>
-                            <div className={style.wrapperNavigation}>
-                                diagramme Component here
-                </div>
-                        </div>
-                        <div className={style.wrapperFS}>
-                            <div className={style.filterWrapper}>
-                                <MonthSelect />
-                                <YearSelect />
-                            </div>
-                            <div className={style.statistic}>
-                                statistics Component here
-                </div>
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-            {props.children}
-            </Container>
-        </React.Fragment >
-    );
-};
-
-
-
+  return (
+    <>
+      <CssBaseline />
+      <React.Fragment>
+        <div className={style.mainWrapper}>
+          <div className={style.header}>Here heder component</div>
+          <div className={style.wrapper}>
+            <div className={style.wrapperSidebar}>
+              <div className={style.wrapperNavigation}>
+                diagramme Component here
+              </div>
+            </div>
+            <div className={style.wrapperFS}>
+              <div className={style.filterWrapper}>
+                <MonthSelect />
+                <YearSelect />
+              </div>
+              <div className={style.statistic}>statistics Component here</div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    </>
+  );
+}
