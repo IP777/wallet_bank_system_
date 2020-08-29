1919e387f3fcd650d671a380b2f30453ecd984e6
IP777 <ivanov_piter@ukr.net>
2020-08-29 15:47:53 +0300

    del commit

--- src/containers/Header.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Header from '../components/Header/Header';
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
