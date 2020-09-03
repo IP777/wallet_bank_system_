268e70356af2884a7788246f8de88661f00f9562
IP777 <ivanov_piter@ukr.net>
2020-09-03 16:21:10 +0300

    transaction edits

--- src/containers/ModalPage.js
+++ src/containers/ModalPage.js
@@ -1,12 +1,7 @@
 import { connect } from 'react-redux';
 import { postTransactions } from '../redux/operations/domain/transaction';
-//import { transactionsSelector } from '../redux/reducers/domain/transactions';
 
 import ModalPage from '../pages/ModalPage/ModalPage';
-
-// const mapStateToProps = (state) => ({
-//   transactions: transactionsSelector(state),
-// });
 
 const mapDispatchToProps = {
   postTransactions,
